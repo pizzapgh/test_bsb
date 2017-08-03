14947fe827ff0ce6851c72740804c74d217ff17c
Chris <chrisbrennanpgh@gmail.com>
2017-08-03 14:45:48 -0400

    add code sticky nav

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+
+$(document).ready(function(){
+
+  // Even when the window is resized, run this code.
+  $(window).resize(function(){
+
+    // Variables
+    var windowHeight = $(window).height();
+
+    // Find the value of 90% of the viewport height
+    var ninetypercent = .8 * windowHeight;
+
+    // When the document is scrolled ninety percent, toggle the classes
+    // Does not work in iOS 7 or below
+    // Hasn't been tested in iOS 8
+    $(document).scroll(function(){
+
+      // Store the document scroll function in a variable
+      var y = $(this).scrollTop();
+
+      // If the document is scrolled 90%
+      if( y > ninetypercent) {
+
+        // Add the "sticky" class
+        $('.bottom-bar').addClass('sticky');
+      } else {
+        // Else remove it.
+        $('.bottom-bar').removeClass('sticky');
+      }
+    });
+
+  // Call it on resize.
+  }).resize();
+
+}); // jQuery
+
 function initMap() {
 
 	var southside = {
@@ -420,7 +456,6 @@ function initMap() {
     };
 
   var map = new google.maps.Map(document.getElementById('map'), options);
-
 
 	var infowindow = new google.maps.InfoWindow({});
 
